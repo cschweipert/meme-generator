cbb0d08049994fcaafd58f860abb11f8af2bd36a
Christina Schweipert <c.schweipert@gmail.com>
2021-01-31 13:25:32 -0700

    Format. Refactor. Works as expected.

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
 const form = document
-  .getElementById("form")
-  .addEventListener("submit", createMeme);
+  .getElementById('form')
+  .addEventListener('submit', createMeme);
 
-  let arr = [];
+let obj = {};
+let arr = [];
 
-  function createMeme(event) {
-    alert('you clicked a button!');
+function createMeme(event) {
+  alert('you clicked a button!');
 
-    const imageLink = document.getElementById('imglink').value;
-    const topText = document.getElementById('toptext').value;
-    const bottomText = document.getElementById('bottomtext').value;
+  const imageLink = document.getElementById('imglink').value;
+  const topText = document.getElementById('toptext').value;
+  const bottomText = document.getElementById('bottomtext').value;
+  let id = 0;
 
-    arr.push(imageLink);
-    console.log(arr);
-  
+  obj.id = id + 1;
+  obj.imageLink = imageLink;
+  obj.topText = topText;
+  obj.bottomText = bottomText;
 
-    let output = document.querySelector('#output');
-  
-    let memediv = output.appendChild(document.createElement('div'));
-    memediv.id = 'memediv'
-    
-    let topdiv = memediv.appendChild(document.createElement('div'));
-    topdiv.id = 'topdiv';
+  arr.push(obj);
+  console.log(arr);
 
-    let image = memediv.appendChild(document.createElement('img'));
-    image.src = imageLink;
-    image.className = 'image';
-    
-    console.log(image);
-  
-  
-    let bottomdiv = document.createElement('div');
-    bottomdiv.id = 'bottomdiv';
-    document.querySelector('.image').appendChild(bottomdiv);
-  
-    let top = document.createElement('h1');
-    top.innerHTML = topText;
-    top.className = 'text';
-    document.getElementById('topdiv').appendChild(top);
-  
-    
-    let bottom = document.createElement('h1');
-    bottom.innerHTML = bottomText;
-    bottom.className = 'text';
-    document.querySelector('.image').appendChild(bottom);
-  
-    event.preventDefault();
-  }
+  let output = document.querySelector('#output');
+
+  let memediv = output.appendChild(document.createElement('div'));
+  memediv.id = 'memediv';
+
+  let top = memediv.appendChild(document.createElement('h1'));
+  top.id = 'top';
+  top.innerHTML = obj.topText;
+  top.className = 'text';
+
+  let image = memediv.appendChild(document.createElement('img'));
+  image.src = obj.imageLink;
+  image.className = 'image';
+
+  let bottom = memediv.appendChild(document.createElement('h1'));
+  bottom.id = 'bottom';
+  bottom.innerHTML = obj.bottomText;
+  bottom.className = 'text';
+
+  event.preventDefault();
+}
