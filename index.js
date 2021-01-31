c58a05e2bfb484f136fbfe146d6d1d1e93e26766
Christina Schweipert <c.schweipert@gmail.com>
2021-01-31 12:33:16 -0700

    Refactor. Create image array.

--- index.js
+++ index.js
@@ -2,44 +2,48 @@ const form = document
   .getElementById("form")
   .addEventListener("submit", createMeme);
 
+  let arr = [];
+
   function createMeme(event) {
     alert('you clicked a button!');
-  
+
     const imageLink = document.getElementById('imglink').value;
     const topText = document.getElementById('toptext').value;
     const bottomText = document.getElementById('bottomtext').value;
-  
-    let memediv = document.createElement('div');
-    memediv.id = 'memediv'
-    document.getElementById('output').appendChild(memediv);
-  
-    let topdiv = document.createElement('div');
-    topdiv.id = 'topdiv';
-    document.querySelector('#memediv').appendChild(topdiv);
 
-    // var c = document.querySelector('#mainContent');
-    // c.appendChild(document.createElement('div'));
+    arr.push(imageLink);
+    console.log(arr);
+  
+
+    let output = document.querySelector('#output');
+  
+    let memediv = output.appendChild(document.createElement('div'));
+    memediv.id = 'memediv'
+    
+    let topdiv = memediv.appendChild(document.createElement('div'));
+    topdiv.id = 'topdiv';
+
+    let image = memediv.appendChild(document.createElement('img'));
+    image.src = imageLink;
+    image.className = 'image';
+    
+    console.log(image);
+  
   
     let bottomdiv = document.createElement('div');
     bottomdiv.id = 'bottomdiv';
-    document.querySelector('#memediv').appendChild(bottomdiv);
+    document.querySelector('.image').appendChild(bottomdiv);
   
     let top = document.createElement('h1');
     top.innerHTML = topText;
     top.className = 'text';
     document.getElementById('topdiv').appendChild(top);
   
-    let image = document.createElement('img');
-    image.src = imageLink;
-    image.className = 'image';
-    document.getElementById('memediv').appendChild(image);
-  
-    console.log(image);
-  
+    
     let bottom = document.createElement('h1');
     bottom.innerHTML = bottomText;
     bottom.className = 'text';
-    document.getElementById('bottomdiv').appendChild(bottom);
+    document.querySelector('.image').appendChild(bottom);
   
     event.preventDefault();
   }
