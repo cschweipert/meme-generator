024b83bec8f68479a360a12d60fb28c67994a347
Christina Schweipert <c.schweipert@gmail.com>
2021-02-01 17:15:12 -0700

    refactor with window.onload and local storage - everything works!!

--- index.js
+++ index.js
@@ -1,79 +1,83 @@
-const form = document
-  .getElementById('form')
-  .addEventListener('submit', createMeme);
+window.onload = function () {
+    const form = document.getElementById('form');
+    const output = document.getElementById('output');
+    
+    //get it from localStorage
+    let savedMemes = JSON.parse(localStorage.getItem('memes')) || [];
+    for (let i = 0; i < savedMemes.length; i++) {
+      let memediv = document.createElement('div');
+      memediv.id = 'memediv';
+  
+      let top = memediv.appendChild(document.createElement('h1'));
+      top.id = 'top';
+      top.innerHTML = savedMemes[i].topText;
+      top.className = 'text';
+      
+      let image = memediv.appendChild(document.createElement('img'));
+      image.src = savedMemes[i].imageLink;
+      image.className = 'image';
+  
+      let bottom = memediv.appendChild(document.createElement('h1'));
+      bottom.id = 'bottom';
+      bottom.innerHTML = savedMemes[i].bottomText;
+      bottom.className = 'text';
+  
+      let deleteButton = memediv.appendChild(document.createElement('button'));
+      deleteButton.className = 'delete-button';
+      deleteButton.innerHTML = 'x';
+      deleteButton.addEventListener('click', deleteMeme);
+  
+      output.appendChild(memediv);
+    }
+  
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+  
+      const imageLink = document.getElementById('imglink').value;
+      const topText = document.getElementById('toptext').value;
+      const bottomText = document.getElementById('bottomtext').value;
+  
+      let output = document.querySelector('#output');
+  
+      let memediv = output.appendChild(document.createElement('div'));
+      memediv.id = 'memediv';
+  
+      let top = memediv.appendChild(document.createElement('h1'));
+      top.id = 'top';
+      top.innerHTML = topText;
+      top.className = 'text';
+  
+      let image = memediv.appendChild(document.createElement('img'));
+      image.src = imageLink;
+      image.className = 'image';
+  
+      let bottom = memediv.appendChild(document.createElement('h1'));
+      bottom.id = 'bottom';
+      bottom.innerHTML = bottomText;
+      bottom.className = 'text';
+  
+      let deleteButton = memediv.appendChild(document.createElement('button'));
+      deleteButton.className = 'delete-button';
+      deleteButton.innerHTML = 'x';
+      deleteButton.addEventListener('click', deleteMeme);
+  
+      form.reset();
+        
+      //save it to localStorage
+      savedMemes.push({
+        imageLink: imageLink,
+        topText: topText,
+        bottomText: bottomText,
+      });
+      localStorage.setItem('memes', JSON.stringify(savedMemes));
+    });
+  };
 
-let imageLink;
-let topText;
-let bottomText;
-
-// const imageLink = document.getElementById('imglink').value;
-// const topText = document.getElementById('toptext').value;
-// const bottomText = document.getElementById('bottomtext').value;
-
-function myReset() {
-  form.reset();
-}
-
-let obj = {};
-let arr = [];
-
-function createMeme(event) {
-  // alert('you clicked a button!');
-
-  // const imageLink = document.getElementById('imglink').value;
-  // const topText = document.getElementById('toptext').value;
-  // const bottomText = document.getElementById('bottomtext').value;
-
-  imageLink = document.getElementById('imglink').value;
-  topText = document.getElementById('toptext').value;
-  bottomText = document.getElementById('bottomtext').value;
-
-  obj.imageLink = imageLink;
-  obj.topText = topText;
-  obj.bottomText = bottomText;
-
-  localStorage.setItem('imageLink', imageLink);
-  localStorage.setItem('topText', topText);
-  localStorage.setItem('bottomText', bottomText);
-
-  let useLink = localStorage.getItem('imageLink');
-  let useTop = localStorage.getItem('topText');
-  let useBottom = localStorage.getItem('bottomText');
-
-  arr.push(obj);
-  console.log(arr);
-
-  let output = document.querySelector('#output');
-
-  let memediv = output.appendChild(document.createElement('div'));
-  memediv.id = 'memediv';
-
-  let top = memediv.appendChild(document.createElement('h1'));
-  top.id = 'top';
-  top.innerHTML = useTop;
-  top.className = 'text';
-
-  let image = memediv.appendChild(document.createElement('img'));
-  image.src = useLink;
-  image.className = 'image';
-
-  let bottom = memediv.appendChild(document.createElement('h1'));
-  bottom.id = 'bottom';
-  bottom.innerHTML = useBottom;
-  bottom.className = 'text';
-
-  let deleteButton = memediv.appendChild(document.createElement('button'));
-  deleteButton.className = 'delete-button';
-  deleteButton.innerHTML = 'x';
-  deleteButton.addEventListener('click', deleteMeme);
-
-  event.preventDefault();
-}
-
-function deleteMeme(event) {
-  // alert('you deleted a meme');
-
-  let output = document.querySelector('#output');
-  let del = document.getElementById('memediv');
-  output.removeChild(del);
-}
+  function deleteMeme(event) {
+    // alert('you deleted a meme');
+  
+    let output = document.querySelector('#output');
+    let del = document.getElementById('memediv');
+    output.removeChild(del);
+  }
+  
